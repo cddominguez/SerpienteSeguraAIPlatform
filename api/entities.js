ecac3a1c257043306f4e5090f564e4ea8aa2e56e
SerpienteSegura <179836487+cddominguez@users.noreply.github.com>
2025-07-28 18:58:53 -0500

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,87 @@
+import { base44 } from './base44Client';
+
+
+export const BackupJob = base44.entities.BackupJob;
+
+export const DLPEvent = base44.entities.DLPEvent;
+
+export const FirewallRule = base44.entities.FirewallRule;
+
+export const AIModel = base44.entities.AIModel;
+
+export const GeoThreat = base44.entities.GeoThreat;
+
+export const NewsItem = base44.entities.NewsItem;
+
+export const OffensiveAction = base44.entities.OffensiveAction;
+
+export const Threat = base44.entities.Threat;
+
+export const SecurityEvent = base44.entities.SecurityEvent;
+
+export const Device = base44.entities.Device;
+
+export const Incident = base44.entities.Incident;
+
+export const DevicePolicy = base44.entities.DevicePolicy;
+
+export const DeviceAlert = base44.entities.DeviceAlert;
+
+export const UserActivity = base44.entities.UserActivity;
+
+export const AccessPolicy = base44.entities.AccessPolicy;
+
+export const UserRiskProfile = base44.entities.UserRiskProfile;
+
+export const ComplianceFramework = base44.entities.ComplianceFramework;
+
+export const SecurityControl = base44.entities.SecurityControl;
+
+export const AuditLog = base44.entities.AuditLog;
+
+export const ComplianceProfile = base44.entities.ComplianceProfile;
+
+export const ComplianceTask = base44.entities.ComplianceTask;
+
+export const DataInventory = base44.entities.DataInventory;
+
+export const ConsentRecord = base44.entities.ConsentRecord;
+
+export const Deployment = base44.entities.Deployment;
+
+export const DoraMetrics = base44.entities.DoraMetrics;
+
+export const ComplianceCheck = base44.entities.ComplianceCheck;
+
+export const VulnerabilityScan = base44.entities.VulnerabilityScan;
+
+export const RealTimeVisitor = base44.entities.RealTimeVisitor;
+
+export const VisitorAuditLog = base44.entities.VisitorAuditLog;
+
+export const ThreatIntelligence = base44.entities.ThreatIntelligence;
+
+export const AIActionAudit = base44.entities.AIActionAudit;
+
+export const HuntingAnnotation = base44.entities.HuntingAnnotation;
+
+export const ExecutiveSummary = base44.entities.ExecutiveSummary;
+
+export const SBOMComponent = base44.entities.SBOMComponent;
+
+export const QuantumThreat = base44.entities.QuantumThreat;
+
+export const IoTDevice = base44.entities.IoTDevice;
+
+export const OTIncident = base44.entities.OTIncident;
+
+export const VoiceCommand = base44.entities.VoiceCommand;
+
+export const FederatedLearningJob = base44.entities.FederatedLearningJob;
+
+export const RiskScenario = base44.entities.RiskScenario;
+
+
+
+// auth sdk:
+export const User = base44.auth;
