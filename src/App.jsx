27258cf4610afe42e0c5834da8c20d2b44e28c1e
SerpienteSegura <179836487+cddominguez@users.noreply.github.com>
2025-07-29 07:31:17 -0500

    Fix App.css import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import '../App.css'
 import Pages from "@/pages/index.jsx"
 import "./App.css";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
     <>
       <Pages />
+      <Toaster />
       <Toaster />
     </>
   )
